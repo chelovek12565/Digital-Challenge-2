2bcfce3bd1fca5bd35b09d1ee78bbb16af19452d
chelovek12565 <bas180407@mail.ru>
2022-11-27 09:37:47 +0500

    doing login

---
+++ bd/user.py
@@ -0,0 +1,14 @@
+import SqlAlchemyBase
+from sqlalchemy_serializer import SerializerMixin
+from .db_session import SqlAlchemyBase
+
+
+class User(SqlAlchemyBase, SerializerMixin):
+    __tablename__ = 'users'
+    user_id = sqlalchemy.Column(sqlalchemy.Integer, unique=True, primary_key=True, autoincrement=True)
+    username = sqlalchemy.Column(sqlalchemy.Text)
+    first_name = sqlalchemy.Column(sqlalchemy.Text)
+    second_name = sqlalchemy.Column(sqlalchemy.Text)
+    email = sqlalchemy.Column(sqlalchemy.Text)
+    password = sqlalchemy.Column(sqlalchemy.Text)
+    user_type = sqlalchemy.Column(sqlalchemy.Text)
--- main.py
+++ main.py
@@ -1,13 +1,41 @@
 from flask import Flask, render_template
+from flask_login import LoginManager
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from bd import bd_session
 import os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "SUPER_MEGA_ULTRA_HYPER_SECRET_KEY"
+
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Логин', validators=[DataRequired()])
+    password = StringField('Пароль', validators=[DataRequired()])
+
+
+@app.route('/login')
+def login_page():
+    form = LoginForm()
+    if form.validate_on_submit():
+        login = form.username.data 
+        password = form.username.data
 
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
 
 
 if __name__ == '__main__':
